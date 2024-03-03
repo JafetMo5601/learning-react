15effb78889231fd09984dbcc0541a63e5612ba8
JafetMo5601 <jafet.moraugalde15@gmail.com>
2024-03-03 04:37:02 -0600

    Finishing the solution for second react project

--- react-essentials/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ react-essentials/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,18 +1,6 @@
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-]
 
-export default function GameBoard({ onSelectSquare, turns }) {
-    let gameBoard = initialGameBoard;
 
-    for (let turn of turns) {
-        const { square, player } = turn;
-        const { row, col } = square;
-
-        gameBoard[row][col] = player;
-    }
+export default function GameBoard({ onSelectSquare, gameBoard }) {
     // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
     // function handleSelectSquare(rowIndex, colIndex) {
