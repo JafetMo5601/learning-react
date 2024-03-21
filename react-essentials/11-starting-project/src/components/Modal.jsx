54cf4db95c20cb810e2243c28ac969ab2a8960e8
JafetMo5601 <jafet.moraugalde15@gmail.com>
2024-03-21 11:55:12 -0600

    New study project to start

---
+++ react-essentials/11-starting-project/src/components/Modal.jsx
@@ -0,0 +1,15 @@
+import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { createPortal } from 'react-dom';
+
+function Modal({ open, children }) {
+  const dialog = useRef();
+
+  return createPortal(
+    <dialog className="modal" ref={dialog} open={open}>
+      {children}
+    </dialog>,
+    document.getElementById('modal')
+  );
+};
+
+export default Modal;
