1c86f7ff0f4931b54efd5200d8022742a6ae4f00
JafetMo5601 <jafet.moraugalde15@gmail.com>
2024-03-23 12:27:31 -0600

    Create an app to practice useEffect and other new knowledge

---
+++ react-essentials-practices/03-starting-project/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header.jsx'
+import Quiz from './components/Quiz.jsx';
+
+function App() {
+    return (
+        <>
+            <Quiz></Quiz>
+            <Header></Header>
+        </>
+    )
+}
+
+export default App;
